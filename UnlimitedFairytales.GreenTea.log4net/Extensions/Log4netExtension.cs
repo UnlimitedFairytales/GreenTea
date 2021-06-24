93d1bf3db67d5d6c93ad5ac49432db97d5f8e76b
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-06-24 14:22:28 +0900

    log4net extension追加

---
+++ UnlimitedFairytales.GreenTea.log4net/Extensions/Log4netExtension.cs
@@ -0,0 +1,35 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace UnlimitedFairytales.GreenTea.log4net.Extensions
+{
+    public static class Log4netExtension
+    {
+        static Log4netExtension()
+        {
+            loggerAssembly = Assembly.GetEntryAssembly();
+            var loggerRepository = LogManager.GetRepository(loggerAssembly);
+            var fileInfo = new FileInfo("log4net.config.xml");
+            XmlConfigurator.Configure(loggerRepository, fileInfo);
+        }
+
+        private static Assembly loggerAssembly;
+
+        public static ILog GetLogger(this object obj, string loggerName)
+        {
+            return LogManager.GetLogger(loggerAssembly, loggerName);
+        }
+
+        public static ILog GetLogger(this object obj, Type type = null)
+        {
+            if (type == null)
+            {
+                type = obj.GetType();
+            }
+            return LogManager.GetLogger(type);
+        }
+    }
+}
---
+++ UnlimitedFairytales.GreenTea.log4netTests/Extensions/Log4netExtensionTests.cs
@@ -0,0 +1,75 @@
+﻿using log4net;
+using System;
+using System.Reflection;
+using Xunit;
+
+namespace UnlimitedFairytales.GreenTea.log4net.Extensions.Tests
+{
+    public class Log4netExtensionTests
+    {
+        class DummyClass
+        {
+        }
+
+        class DummyClass2
+        {
+        }
+
+        [Fact()]
+        public void GetLoggerTest_Parameter()
+        {
+            {
+                // Arrange
+                // log4net.config.xml
+                var obj = new DummyClass();
+                // Act
+                var logger = Log4netExtension.GetLogger(obj, "fooLogger");
+                // Assert
+                Assert.Equal("fooLogger", logger.Logger.Name);
+                Assert.Equal(LogManager.GetRepository(Assembly.GetEntryAssembly()), logger.Logger.Repository);
+            }
+            {
+                // Arrange
+                // log4net.config.xml
+                var obj = new DummyClass();
+                // Act
+                var logger = Log4netExtension.GetLogger(obj);
+                // Assert
+                Assert.Equal(typeof(DummyClass).FullName, logger.Logger.Name);
+                Assert.Equal(LogManager.GetRepository(Assembly.GetEntryAssembly()), logger.Logger.Repository);
+            }
+            {
+                // Arrange
+                // log4net.config.xml
+                var obj = new DummyClass();
+                // Act
+                var logger = Log4netExtension.GetLogger(obj, typeof(DummyClass2));
+                // Assert
+                Assert.Equal(typeof(DummyClass2).FullName, logger.Logger.Name);
+                Assert.Equal(LogManager.GetRepository(Assembly.GetEntryAssembly()), logger.Logger.Repository);
+            }
+        }
+
+        [Fact()]
+        public void GetLoggerTest_Logging()
+        {
+            // Arrange
+            // log4net.config.xml
+            var obj = new DummyClass();
+            // Act
+            var logger = Log4netExtension.GetLogger("hoge");
+            // Assert
+            logger.Debug("デバッグ");
+            logger.Info("インフォ");
+            try
+            {
+                throw new Exception("任意のエラー");
+            }
+            catch (Exception ex)
+            {
+                GlobalContext.Properties["UserId"] = "12345";
+                logger.Error("メッセージ", ex);
+            }
+        }
+    }
+}
