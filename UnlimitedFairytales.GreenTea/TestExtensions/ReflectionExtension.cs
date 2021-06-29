e0ab33de05269a2587a5f4170bd86286347ed340
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-06-29 19:10:07 +0900

    ReflectionSetField追加

---
+++ UnlimitedFairytales.GreenTea/TestExtensions/ReflectionExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnlimitedFairytales.GreenTea.TestExtensions
+{
+    public static class ReflectionExtension
+    {
+        public static void ReflectionSetField(this object obj, string fieldName, object value)
+        {
+            var info = obj.GetType().GetField(fieldName);
+            info.SetValue(obj, value);
+        }
+    }
+}
---
+++ UnlimitedFairytales.GreenTeaTests/TestExtensions/ReflectionExtensionTests.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+
+namespace UnlimitedFairytales.GreenTea.TestExtensions.Tests
+{
+    public class ReflectionExtensionTests
+    {
+        public readonly string ReadonlyFieldSample = "foo";
+
+        [Fact()]
+        public void ReflectionSetFieldTest()
+        {
+            // Arrange
+            var obj = new ReflectionExtensionTests();
+
+            // Act
+            obj.ReflectionSetField(nameof(obj.ReadonlyFieldSample), "bar");
+
+            // Assert
+            Assert.Equal("bar", obj.ReadonlyFieldSample);
+        }
+    }
+}
