725e4a9db0715bcb9592cd06f54f0db5ede9dde1
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-09-04 02:45:26 +0900

    csvHelperのサンプル追加

---
+++ UnlimitedFairytales.GreenTea.CsvHelper/Extensions/CsvHelperExtension.cs
@@ -0,0 +1,103 @@
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnlimitedFairytales.GreenTea.CsvHelper.Extensions
+{
+    public static class CsvHelperExtension
+    {
+        public static readonly Encoding SJIS_WIN;
+        public static readonly Encoding EUC_JP;
+        public static readonly Encoding ISO_2022_JP;
+        public static readonly Encoding UTF8;
+
+        static CsvHelperExtension()
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            SJIS_WIN = Encoding.GetEncoding(932);
+            EUC_JP = Encoding.GetEncoding(51932);
+            ISO_2022_JP = Encoding.GetEncoding(50220);
+            UTF8 = Encoding.GetEncoding(65001);
+        }
+
+        public static CsvConfiguration GetCsvConfiguration(Encoding enc, BadDataFound badDataFound = null)
+        {
+            // https://github.com/JoshClose/CsvHelper/blob/27.1.1/src/CsvHelper/Configuration/CsvConfiguration.cs
+            var cultureInfo = CultureInfo.InvariantCulture;
+            var config = new CsvConfiguration(cultureInfo)
+            {
+                // var 27.1.1
+                //AllowComments = false,
+                AllowComments = true,
+                //BadDataFound = ConfigurationFunctions.BadDataFound,
+                //BufferSize = 0x1000, // 4096
+                //CacheFields = false,
+                //Comment = '#',
+                //CountBytes = false,
+                //CultureInfo
+                //Delimiter = cultureInfo.TextInfo.ListSeparator, // ","
+                //DetectDelimiter = false,
+                //DetectDelimiterValues = new[] { ",", ";", "|", "\t" },
+                //DetectColumnCountChanges = false,
+                //DynamicPropertySort = null,
+                //Encoding = Encoding.UTF8,
+                Encoding = enc,
+                //Escape = '"',
+                //ExceptionMessagesContainRawData = true,
+                //GetConstructor = ConfigurationFunctions.GetConstructor,
+                //GetDynamicPropertyName = ConfigurationFunctions.GetDynamicPropertyName,
+                //HasHeaderRecord = true,
+                //HeaderValidated = ConfigurationFunctions.HeaderValidated,
+                //IgnoreBlankLines = true,
+                //IgnoreReferences = false,
+                //IncludePrivateMembers = false, // true
+                //InjectionCharacters = new[] { '=', '@', '+', '-' },
+                //InjectionEscapeCharacter = '\t',
+                //IsNewLineSet
+                //LeaveOpen = false,
+                //LineBreakInQuotedFieldIsBadData = false,
+                //MemberTypes = MemberTypes.Properties,
+                //MissingFieldFound = ConfigurationFunctions.MissingFieldFound,
+                //Mode = 0,
+                //NewLine = "\r\n",
+                //PrepareHeaderForMatch = ConfigurationFunctions.PrepareHeaderForMatch,
+                //ProcessFieldBufferSize = 1024,
+                //Quote = '"',
+                //ReadingExceptionOccurred = ConfigurationFunctions.ReadingExceptionOccurred,
+                //ReferenceHeaderPrefix = null,
+                //SanitizeForInjection = false,
+                //ShouldQuote = ConfigurationFunctions.ShouldQuote,
+                //ShouldSkipRecord = ConfigurationFunctions.ShouldSkipRecord,
+                //ShouldUseConstructorParameters = ConfigurationFunctions.ShouldUseConstructorParameters,
+                //TrimOptions = 0,
+                //UseNewObjectForNullReferenceMembers = true,
+                //WhiteSpaceChars = new char[] { ' ' },
+            };
+            if (badDataFound != null)
+            {
+                config.BadDataFound = badDataFound;
+            }
+            return config;
+        }
+
+        public static List<T> ReadCsv<T>(this string filePath, Encoding enc, CsvConfiguration config = null)
+        {
+            if (config == null)
+            {
+                config = CsvHelperExtension.GetCsvConfiguration(enc);
+            }
+            using (var sr = new StreamReader(filePath, config.Encoding))
+            using (var reader = new CsvReader(sr, config))
+            {
+                return reader.GetRecords<T>().ToList();
+            }
+        }
+    }
+}
---
+++ UnlimitedFairytales.GreenTea.CsvHelperTests/Extensions/CsvHelperExtensionTests.cs
@@ -0,0 +1,66 @@
+﻿using Xunit;
+using UnlimitedFairytales.GreenTea.CsvHelper.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
+using CsvHelper;
+
+namespace UnlimitedFairytales.GreenTea.CsvHelper.Extensions.Tests
+{
+    public class AccountCsvDto
+    {
+        [Name("ID")]
+        public long? Id { get; set; }
+        [Name("名前")]
+        public string Name { get; set; }
+        [Name("E-Mail")]
+        public string EMail { get; set; }
+        [Name("年齢")]
+        public int Age { get; set; }
+        [Name("1 備考"), NameIndex(0)]
+        public string Note { get; set; }
+        [Name("1 備考"), NameIndex(1)]
+        public string Note2 { get; set; }
+        [Name("1 備考"), NameIndex(2)]
+        public string Note3 { get; set; }
+    }
+
+    public class CsvHelperExtensionTests
+    {
+        [Fact()]
+        public void ReadCsvTest()
+        {
+            {
+                // Arrange
+                var path = "./sjis-csv-sample.csv";
+                // Act
+                var accounts = path.ReadCsv<AccountCsvDto>(CsvHelperExtension.SJIS_WIN).ToList();
+                // Assert
+                Assert.Equal(4, accounts.Count);
+                Assert.Equal("Alice", accounts[0].Name);
+                Assert.Equal("alice@foo.com", accounts[0].EMail);
+                Assert.Equal(18, accounts[0].Age);
+                Assert.Equal("note", accounts[0].Note);
+                Assert.Equal("note2", accounts[0].Note2);
+                Assert.Equal("note3", accounts[0].Note3);
+            }
+            {
+                // Arrange
+                var path = "./sjis-csv-badsample.csv";
+                // Act
+                try
+                {
+                    path.ReadCsv<AccountCsvDto>(CsvHelperExtension.SJIS_WIN).ToList();
+                }
+                catch (BadDataException ex)
+                {
+                    // Assert
+                    Assert.Contains(@"4,Dave,""dave@qu""x.com"",20,""no,""te"",""note2"",""note3""", ex.Message);
+                }
+            }
+        }
+    }
+}
