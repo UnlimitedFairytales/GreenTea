9956df600cc0c3fffa063a0559c3fa02656228f3
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2021-09-04 00:35:47 +0900

    リネームとコメント追記

--- UnlimitedFairytales.GreenTea.log4net/Extensions/Log4netExtension.cs
+++ UnlimitedFairytales.GreenTea.log4net/Extensions/Log4netExtension.cs
@@ -6,21 +6,25 @@ using System.Reflection;
 
 namespace UnlimitedFairytales.GreenTea.log4net.Extensions
 {
+    /// <summary>
+    /// .NET Core系統はAppDomainの追加をサポートしないため、Assembly.GetEntryAssembly()は実行を開始したexeであることが保証される。
+    /// https://docs.microsoft.com/ja-jp/dotnet/core/porting/net-framework-tech-unavailable
+    /// </summary>
     public static class Log4netExtension
     {
         static Log4netExtension()
         {
-            loggerAssembly = Assembly.GetEntryAssembly();
-            var loggerRepository = LogManager.GetRepository(loggerAssembly);
+            entryPointAssembly = Assembly.GetEntryAssembly();
+            var loggerRepository = LogManager.GetRepository(entryPointAssembly);
             var fileInfo = new FileInfo("log4net.config.xml");
             XmlConfigurator.Configure(loggerRepository, fileInfo);
         }
 
-        private static Assembly loggerAssembly;
+        private static Assembly entryPointAssembly;
 
         public static ILog GetLogger(this object obj, string loggerName)
         {
-            return LogManager.GetLogger(loggerAssembly, loggerName);
+            return LogManager.GetLogger(entryPointAssembly, loggerName);
         }
 
         public static ILog GetLogger(this object obj, Type type = null)
